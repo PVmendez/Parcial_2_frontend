414e6a1f70332c6d293b991c291b0c2bc9ba2035
PVmendez <valentino.mendez.rey@gmail.com>
2023-07-03 09:06:40 -0300

    fix: fix project name

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'prueba_parcial';
+  title = 'prueba_parcial_frontend';
 }
